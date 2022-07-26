cf60046359795e97c7a9abf76b9536a9e057b684
Young il Cho <0young@kakao.com>
2022-07-26 18:44:31 +0900

    Inception patch

--- srcs/requirements/mariadb/tools/create_user.sh
+++ srcs/requirements/mariadb/tools/create_user.sh
@@ -2,8 +2,8 @@
 
 service mysql start
 
-echo "CREATE DATABASE ${DB_NAME};" | mysql -u root -e
-echo "CREATE USER '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';" | mysql -u root -e
-echo "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%';" | mysql -u root -e 
-echo "FLUSH PRIVILEGES;" | mysql -u root -e
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';" | mysql -u root -e
+echo "CREATE DATABASE ${DB_NAME};" | mysql -u root
+echo "CREATE USER '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';" | mysql -u root
+echo "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%';" | mysql -u root
+echo "FLUSH PRIVILEGES;" | mysql -u root
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';" | mysql -u root
--- srcs/requirements/wordpress/tools/install_wp.sh
+++ srcs/requirements/wordpress/tools/install_wp.sh
@@ -1,20 +1,23 @@
 #!bin/bash
 
+mkpir -p /var/www/wordpress
+chown -R www-data:www-data /var/www/wordpress
+chown www-data:www-data /var/www/wordpress
 cd /var/www/wordpress
+sudo -u www-data wp core download
+
+sudo -u www-data \
 wp core config	--dbhost=$DB_HOST \
 				--dbname=$DB_NAME \
 				--dbuser=$DB_USER \
 				--dbpass=$DB_PASSWORD \
-				--allow-root
 
+sudo -u www-data \
 wp core install --title=$WP_TITLE \
 				--admin_user=$WP_ADMIN_USER \
 				--admin_password=$WP_ADMIN_PASSWORD \
 				--admin_email=$WP_ADMIN_MAIL \
 				--url=$WP_URL \
-				--allow-root
 
-wp user create $WP_USER $WP_USER_MAIL --role=author --user_pass=$WP_USER_PASSWORD --allow-root
-cd -
-
-php-fpm7.3 -F
+sudo -u www-data \
+wp user create $WP_USER $WP_USER_MAIL --role=author --user_pass=$WP_USER_PASSWORD
